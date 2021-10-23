eb679bac68ecd8e5bcbf68ff78c5b29b12b90b92
theAmbientOrange <therealjameswu@gmail.com>
2021-10-23 18:00:20 -0400

    Added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Calendar } from './Calendar/Calendar';
+import { TabLayout } from './Layouts/TabLayout';
 
 function App() {
   return (
     <div className="App">
-      <Calendar />
+      <TabLayout />
     </div>
   );
 }
